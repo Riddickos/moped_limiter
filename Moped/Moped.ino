c0847ea8f1f45e7f9ef0edf9c863b85ef11208b2
Sergey Potapov <SePotapov@luxoft.com>
2022-10-05 15:39:48 +0200

    Add hardware timer

--- Moped/Moped.ino
+++ Moped/Moped.ino
@@ -1,34 +1,35 @@
 #include <SoftwareSerial.h>
 #include <EEPROM.h>
+#include "timer1.h"
+
 SoftwareSerial ble_device(4, 3); // RX | TX
 #define relayPin A1
 
-int rpm;
+volatile unsigned long revolutionsCounter = 0;
+volatile unsigned long revolutionsResult = 0;
+
 int limit_rpm;
-int kill_delay;
-unsigned long timeold;
-bool revolution = false;
 bool isLimitOn = true;
 bool isDebug = false;
-String str_ii = "";
+int duration = 0;
 
 void setup()
 {
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, HIGH);
-  pinMode(relayPin, OUTPUT);
-  rpm = 0;
-  timeold = 0;
-  pinMode(2, INPUT_PULLUP);
-  attachInterrupt(0, magnet_detect, RISING); //Initialize the intterrupt pin (Arduino digital pin 2)
-  limit_rpm = readIntFromEEPROM(1);
-  kill_delay = readIntFromEEPROM(3);
-  ble_device.begin(9600);
-  digitalWrite(LED_BUILTIN, LOW);
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, HIGH);
+    pinMode(relayPin, OUTPUT);
+    pinMode(2, INPUT_PULLUP);
+    attachInterrupt(0, magnet_detect, RISING); //Initialize the intterrupt pin (Arduino digital pin 2)
+    limit_rpm = readIntFromEEPROM(1);
+    duration = readIntFromEEPROM(3);
+    ble_device.begin(9600);
+    digitalWrite(LED_BUILTIN, LOW);
+
+    unsetCoutner();
 }
 
 void magnet_detect(){
-  revolution = true;
+    ++revolutionsCounter;
 }
 
 void writeIntIntoEEPROM(int address, int number)
@@ -46,63 +47,68 @@ void printLimitsStatus()
 {
   ble_device.println("Debug "+String(isDebug));  
   ble_device.println("RPM "+String(limit_rpm));  
-  ble_device.println("Delay "+String(kill_delay));
+  ble_device.println("Duration "+String(duration));
   ble_device.println("Limiter "+String(isLimitOn));
 }
 
 void loop()
 {
-  if (revolution) {
-    rpm = 60000 / (millis() - timeold);
-    timeold = millis();
-    revolution = false;
-  }
+    if ((revolutionsResult > limit_rpm * duration / 1000)) {
+        if (isLimitOn) {
+            digitalWrite(relayPin, LOW);
+            if (isDebug){
+                digitalWrite(LED_BUILTIN, HIGH);
+                ble_device.println("RPM "+String(revolutionsResult));
+            }
+        }
+    } else {
+        digitalWrite(relayPin, HIGH);
+        digitalWrite(LED_BUILTIN, LOW);
+    }
 
-  if ((rpm > limit_rpm)) {
-    if (isLimitOn) {
-      digitalWrite(relayPin, LOW);
-      if (isDebug){
+    if (ble_device.available()) {
+        static String str_ii = "";
+
         digitalWrite(LED_BUILTIN, HIGH);
-        ble_device.println("RPM "+String(rpm));
-      }
-      delay(kill_delay);
+        char in_char = ble_device.read();
+        if (int(in_char) != -1 and int(in_char) != 42) {
+            if (in_char != '\n' and in_char != '\r') {
+                str_ii += in_char;
+            }
+        }
+        if (in_char == '\n') {
+            if (str_ii == "TOGGLE_LIMIT") {
+                isLimitOn = !isLimitOn;
+            }
+            if (str_ii == "TOGGLE_DEBUG") {
+                isDebug = !isDebug;
+            }
+            if (str_ii.startsWith("RPM_")) {
+                limit_rpm = str_ii.substring(4, str_ii.length()).toInt();
+                writeIntIntoEEPROM(1, limit_rpm);
+            }
+            if (str_ii.startsWith("DURATION_")) {
+                duration = str_ii.substring(9, str_ii.length()).toInt();
+                writeIntIntoEEPROM(3, duration);
+            }
+            str_ii = "";
+            printLimitsStatus();
+        }
     }
-  } else {
-    digitalWrite(relayPin, HIGH);
-    digitalWrite(LED_BUILTIN, LOW);
-  }
-
-  if (ble_device.available()) {
-    digitalWrite(LED_BUILTIN, HIGH);
-    char in_char = ble_device.read();
-    if (int(in_char) != -1 and int(in_char) != 42) {
-      if (in_char != '\n' and in_char != '\r') {
-        str_ii += in_char;
-      }
-    }
-    if (in_char == '\n') {
-      if (str_ii == "TOGGLE_LIMIT") {
-        isLimitOn = !isLimitOn;
-      }
-      if (str_ii == "TOGGLE_DEBUG") {
-        isDebug = !isDebug;
-      }
-      if (str_ii.startsWith("RPM_")) {
-        limit_rpm = str_ii.substring(4, str_ii.length()).toInt();
-        writeIntIntoEEPROM(1, limit_rpm);
-      }
-      if (str_ii.startsWith("DELAY_")) {
-        kill_delay = str_ii.substring(6, str_ii.length()).toInt();
-        writeIntIntoEEPROM(3, kill_delay);
-      }
-      if (str_ii.startsWith("LOCK_RELAY_")) {
-        digitalWrite(relayPin, LOW);
-        int delay_time = str_ii.substring(11, str_ii.length()).toInt();
-        delay(delay_time);
-      }
-      printLimitsStatus();
-      str_ii = "";
-    }
-  }
-  rpm = 0;
 }
+
+void unsetCoutner()
+{
+    revolutionsCounter = 0;
+    timer1::instance().init(static_cast<double>(duration) / 1000);
+    timer1::instance().start();
+}
+
+ISR(TIMER1_COMPA_vect)
+{
+    cli();
+    revolutionsResult = revolutionsCounter;
+    unsetCoutner();
+    sei();
+}
+
